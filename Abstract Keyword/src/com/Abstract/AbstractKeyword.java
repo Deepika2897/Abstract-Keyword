e2b679b45185cd1a1a1627ccaea648e9cfa72fd8
vijay <vijay@DESKTOP-6315K2E>
2022-03-27 20:13:10 +0530

    Using Abstract

---
+++ Abstract Keyword/src/com/Abstract/AbstractKeyword.java
@@ -0,0 +1,42 @@
+package com.Abstract;
+//abstract: class and method
+//if you don't want object to be created then declare class as abstract
+//for any method in the class if you are not implementing the declare method abstract
+//if any method is abstract, the compiler says declare class also abstract
+//if any abstract class, then inheritance is a must
+
+
+abstract class Area {
+	abstract void calArea();//without body
+	Area(){
+		System.out.println("Abstract class can have constructor");
+	}
+	void displ() {//method with the body
+		System.out.println("Method with the body");
+	}
+}
+
+class Reactangle extends Area{
+
+	@Override
+	void calArea() {
+		int l=9;
+		int b=7;
+		int area=l*b;
+		System.out.println("area of reactangle="+area);
+		
+	}
+	
+}
+public class AbstractKeyword {
+
+	public static void main(String[] args) {
+		//Area ob=new Area();//abstract class cannot be instantiated
+		Reactangle ob=new Reactangle();
+		ob.calArea();
+
+	}
+
+}
+
+
